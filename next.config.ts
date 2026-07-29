e1ab903ab74a5b4b3e788c87700ce927c5c65016
reallymohammad <moamiri3322@gmail.com>
2026-07-29 18:23:37 +0330

    fix read image

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default withNextIntl(nextConfig);
